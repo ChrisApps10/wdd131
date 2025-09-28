0164d4318bdd36670b044af2dee975a391d0541e
ChrisApps10 <chrisaparicio18@gmail.com>
2025-09-27 17:33:47 -0700

    fixed temples.html

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,13 +1,10 @@
-// Get the current year for the footer
 const currentYearSpan = document.getElementById("currentyear");
 const today = new Date();
 currentYearSpan.textContent = today.getFullYear();
 
-// Get the last modified date for the footer
 const lastModifiedSpan = document.getElementById("lastmodified");
 lastModifiedSpan.textContent = document.lastModified;
 
-// Hamburger menu functionality
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('#navigation-menu');
 
